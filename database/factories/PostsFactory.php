8672da25099373dcc7adf746c25108f2369cd42d
Tobias L. Perez <tobiasleonel77@gmail.com>
2022-09-10 00:14:59 -0300

    [ADD] Model , Factory , Seeder
    Se agregaron seeders para no tener que estar continuamente generando registros en la db , se pueden agregar desde el tinker como también desde corriendo db:seed.

---
+++ database/factories/PostsFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'titulo' => $this->faker->text(45),
+            'slug' => Str::slug($this->faker->text(15)),
+            'description' => $this->faker->text(900),
+        ];
+    }
+}
