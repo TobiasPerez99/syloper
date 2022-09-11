42680d2a4e1700b38bcf9860f92e0c9a793e105e
Tobias L. Perez <tobiasleonel77@gmail.com>
2022-09-10 23:07:16 -0300

    Create PostRequest.php

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(Request $request)
+    {
+
+        return [
+            'titulo' => 'required',
+            'descripcion' => 'required',
+            'slug' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'titulo.required' => 'El Titulo es Requerido',
+            'slug.required' => 'El slug es Requerido',
+            'descripcion.required' => 'La Descripcion es Requerida',
+        ];
+    }
+}
