ec4310c6a3ee8186a8a0108d9a1dd34d652725d9
Tobias L. Perez <tobiasleonel77@gmail.com>
2022-09-14 01:02:17 -0300

    Update PostController.php

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['index', 'show', 'store' , 'delete']]);
+        $this->middleware('auth:api', ['except' => ['index', 'show', 'store' ,'destroy']]);
     }
 
     /**
